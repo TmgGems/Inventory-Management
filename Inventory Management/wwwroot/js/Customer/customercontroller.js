e3d03ea1b1cca4c7ef3ead3af3a8f9c1a28989c2
Gyalwang Ghhyamjo lama <gyalwanglama123@gmail.com>
2024-07-08 11:45:21 +0545

    Customer CRUD done

---
+++ Inventory Management/wwwroot/js/Customer/customercontroller.js
@@ -0,0 +1,123 @@
+﻿/// <reference path="../knockout.js" />
+
+const mode = {
+    create: 1,
+    update: 2
+};
+var customerController = function () {
+    var self = this;
+    const baseUrl = "/api/CustomerAPI";
+    self.CustomerList = ko.observableArray([]);
+    self.NewCustomer = ko.observable(new customermodel());
+    self.IsUpdated = ko.observable(false);
+    self.SelectedCustomer = ko.observable(new customermodel());
+    self.mode = ko.observable(mode.create);
+
+    //Fetch Data From Server
+    self.getData = function () {
+        ajax.get(baseUrl).then(function (result) {
+            self.CustomerList(result.map(item => new customermodel(item)));
+        });
+    }
+    self.getData();
+
+    self.AddCustomer = function () {
+        switch (self.mode()) {
+            case mode.create:
+                ajax.post(baseUrl, ko.toJSON(self.NewCustomer()))
+                    .done(function (result) {
+                        self.CustomerList.push(new customermodel(result));
+                        self.resetForm();
+                        self.getData();
+                        $('#customerModal').modal('hide');
+                    })
+                    .fail(function (err) {
+                        console.log(err);
+                    });
+                break;
+            case mode.update:
+                ajax.put(baseUrl, ko.toJSON(self.NewCustomer()))
+                    .done(function (result) {
+                        self.CustomerList.replace(self.SelectedCustomer(), new customermodel(result));
+                        self.resetForm();
+                        self.getData();
+                        $('#customerModal').modal('hide');
+                    })
+                    .fail(function (err) {
+                        console.log(err);
+                    });
+                break;
+            default:
+                console.log("Invalid mode");
+        }
+    };
+
+
+    self.DeleteCustomer = function (model) {
+        ajax.delete(baseUrl + "?id=" + model.id())
+            .done((result) => {
+                self.CustomerList.remove(model);
+            })
+            .fail((err) => {
+                console.log(err);
+            });
+    };
+
+
+    self.SelectCustomer = (model) => {
+        self.SelectedCustomer(model);
+        self.NewCustomer(new customermodel(ko.toJS(model)));
+        self.IsUpdated(true);
+        self.mode(mode.update);
+        $('#customerModal').modal('show');
+    }
+
+    self.setCreateMode = function () {
+        self.resetForm();
+        $('#customerModal').modal('show');
+    };
+
+    self.resetForm = () => {
+        self.NewCustomer(new customermodel());
+        self.IsUpdated(false);
+        self.mode(mode.create);
+    };
+
+}
+var ajax = {
+    get: function (url) {
+        return $.ajax({
+            method: "GET",
+            url: url,
+            async: false,
+        });
+    },
+    post: function (url, data) {
+        return $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            url: url,
+            data: (data)
+        });
+    },
+    put: function (url, data) {
+        return $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "PUT",
+            url: url,
+            data: data
+        });
+    },
+    delete: function (route) {
+        return $.ajax({
+            method: "DELETE",
+            url: route,
+        });
+    }
+}
---
+++ Inventory Management/wwwroot/js/Customer/customermodel.js
@@ -0,0 +1,9 @@
+﻿var customermodel = function(item)
+{
+    var self = this;
+    item = item || {};
+    self.id = ko.observable(item.id || 0);
+    self.fullName = ko.observable(item.fullName || '');
+    self.contactNo = ko.observable(item.contactNo || '');
+    self.address = ko.observable(item.address || '');
+}
